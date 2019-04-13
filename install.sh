155a6b1b8be6f9e3c9755ee11be4c00ef53237a4
return314 <noreturn314@gmail.com>
2019-04-13 11:06:57 -0400

    added run script

--- install.sh
+++ install.sh
@@ -1,9 +1,20 @@
-
+#!/bin/bash
 # set path for openBLAS
-export OPENBLAS_HOME=/PATH/TO/OPENBLAS
+# export OPENBLAS_HOME=/PATH/TO/OPENBLAS
 
-# set path for Boost
-export BOOST_HOME=/PATH/TO/BOOST
+# # set path for Boost
+# export BOOST_HOME=/PATH/TO/BOOST
+
+
+# Install MM-CSF
+echo "Isntalling MM-CSF"
+
+cd MM-CSF
+
+make
+
+cd ..
+
 
 # Install SPLATT LIBRARY
 echo "Isntalling SPLATT library"
@@ -11,7 +22,7 @@ echo "Isntalling SPLATT library"
 echo "Downloading SPLATT"
 git clone https://github.com/ShadenSmith/splatt.git
 cd splatt
-./configure --precision=single --with-blas-lib=${LAPACK_LIB_PATH}/lib/libopenblas_haswellp-r0.3.6.dev.so --with-lapack-lib=${LAPACK_LIB_PATH}/lib/libopenblas_haswellp-r0.3.6.dev.so 
+./configure --precision=single --with-blas-lib=${OPENBLAS_HOME}/lib/libopenblas_haswellp-r0.3.6.dev.so --with-lapack-lib=${OPENBLAS_HOME}/lib/libopenblas_haswellp-r0.3.6.dev.so 
 
 make
 
@@ -31,4 +42,18 @@ cd ParTI
 echo "Steps to manually install ParTI are described in the manual_install.txt or in https://github.com/hpcgarage/ParTI"
 
 cd ..
+
+# Install B-CSF LIBRARY
+echo "Isntalling B-CSF"
+
+echo "Downloading B-CSF"
+
+git clone https://github.com/isratnisa/B-CSF.git
+cd B-CSF/src
+make
+
+echo "Steps to manually install B-CSF are described in the manual_install.txt or in https://github.com/isratnisa/B-CSF"
+
+cd ..
+
 
---
+++ run_banchmark.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${OPENBLAS_HOME}/lib
+
+# artifact_home=~/SC19_artifact/mttkrp
+artifact_home=/users/PAS0134/osu1600/test
+path=~/SC19_artifact/mttkrp/datasets
+out=$artifact_home/output.txt
+
+declare -a dataset=("nell-2")
+
+# declare -a dataset=("delicious-3d" "nell-1" "nell-2" "flickr-3d" "freebase_music" "freebase_sampled" "1998DARPA")
+rank=32
+
+for tsr_name in "${dataset[@]}"; do
+
+	if [ ${tsr_name} = "delicious-3d" ]; then
+		nnz=140126181
+		sk=16
+	fi
+	if [ ${tsr_name} = "nell-1" ]; then
+		nnz=143599552
+		sk=18
+	fi
+	if [ ${tsr_name} = "nell-2" ]; then
+		nnz=76879419
+		sk=9
+	fi
+	if [ ${tsr_name} = "flickr-3d" ]; then
+		nnz=112890310
+		sk=13
+	fi
+	if [ ${tsr_name} = "freebase_music" ]; then
+		nnz=99546551
+		sk=16
+	fi
+	if [ ${tsr_name} = "freebase_sampled" ]; then
+		nnz=139920771
+		sk=16
+	fi
+	if [ ${tsr_name} = "1998DARPA" ]; then
+		nnz=28436033
+		sk=15
+	fi
+
+	if [ ${sk} -ge "8" ]; then
+		sb=7
+	else
+		sb=${sk}
+	fi
+
+	echo $dataset >> $out
+
+	# echo "running MM-CSF"
+	bin=$artifact_home/MM-CSF/mttkrp
+	$bin -i $path/"$tsr_name"_wDims.tns -m 0 -R $rank -t 12 -f 128 -w 1 -s 8 -b 256 &> mm_tmp
+	mode1Time=`cat mm_tmp | grep 'mode\ 0' | awk -F':' '{print $2}' |  awk -F',' '{print $1}'`
+	mode2Time=`cat mm_tmp | grep 'mode\ 1' | awk -F':' '{print $2}' |  awk -F' ' '{print $1}'`
+	mode3Time=`cat mm_tmp | grep 'mode\ 2' | awk -F':' '{print $2}' |  awk -F' ' '{print $1}'`
+
+	totTime=`echo "$mode1Time + $mode2Time + $mode3Time" | bc -l`
+	log1=`echo "$totTime * 1" | bc -l`
+	GFLOPS=`echo "9 * $rank * $nnz / ($log1 * 1000000)" | bc -l`
+	echo "MM-CSF,GFLOPS - $GFLOPS" >> $out	
+
+	# bin=$artifact_home/B-CSF/src/mttkrp
+	# log1=`$bin -i $path/"$tsr_name"_wDims.tns -m 0 -R $rank -t 8 -f 128 | perl -p -e 's/\n//'`
+	# echo "B-CSF,$dataset,$log1" >> $out
+
+# SPLATT ALL-CSF
+	echo "Processing SPLATT (ALL-CSF)"
+	bin=$artifact_home/splatt/build/Linux-x86_64/bin/splatt
+	$bin cpd --csf=all $path/"$tsr_name".tns -r $rank --verbose --iters=1 --tile --nowrite -t 28 &> splatt_tmp
+	sptime=`grep 'MTTKRP' splatt_tmp | awk -F' ' '{print $2}'` 
+	log1s="${sptime: : -1}"
+	log1=`echo "$log1s * 1000" | bc -l`
+	GFLOPS=`echo "9 * $rank * $nnz / ($log1 * 1000000)" | bc -l`
+	echo "SPLATT-ALL-CSF,GFLOPS - $GFLOPS" >> $out	
+
+# SPLATT-ONECSF
+	echo "Processing SPLATT (ONE-CSF)"
+	bin=$artifact_home/splatt/build/Linux-x86_64/bin/splatt
+	$bin cpd --csf=one $path/"$tsr_name".tns -r $rank --verbose --iters=1 --tile --nowrite -t 28 &> splatt_tmp
+	sptime=`grep 'MTTKRP' splatt_tmp | awk -F' ' '{print $2}'` 
+	log1s="${sptime: : -1}"
+	log1=`echo "$log1s * 1000" | bc -l`
+	GFLOPS=`echo "9 * $rank * $nnz / ($log1 * 1000000)" | bc -l | scale=2`
+	echo "SPLATT-ONE-CSF,GFLOPS - $GFLOPS" >> $out	
+
+	# bin=$artifact_home/ParTI/build/examples/mttkrp_gpu
+	# for (( m = 0; m < 3; m++ )); do
+	# 	$bin $path/"$tsr_name"_wDims.tns $m 15 0 $rank &> PARTItime
+	# 	log1=`cat PARTItime | grep 'CUDA SpTns MTTKRP' | perl -p -e 's/\n//'`
+	# 	echo "ParTI-GPU,$dataset,$log1" >> $outfile
+	# done
+
+# HiCOO
+	bin=$artifact_home/ParTI/build/examples/mttkrp_hicoo
+    $bin -i $path/"$tsr_name"_wDims.tns -m -1 -b ${sb} -k ${sk}  -d -1 -r $rank  -t 28 &> hicoo_tmp
+
+	mode1Time=`cat hicoo_tmp | grep 'MTTKRP MODE\ 0' | awk -F':' '{print $2}' |  awk -F' ' '{print $1}'`
+	mode2Time=`cat hicoo_tmp | grep 'MTTKRP MODE\ 1' | awk -F':' '{print $2}' |  awk -F' ' '{print $1}'`
+	mode3Time=`cat hicoo_tmp | grep 'MTTKRP MODE\ 2' | awk -F':' '{print $2}' |  awk -F' ' '{print $1}'`
+
+	totTime=`echo "$mode1Time + $mode2Time + $mode3Time" | bc -l`
+	log1=`echo "$totTime * 1000" | bc -l`
+	GFLOPS=`echo "9 * $rank * $nnz / ($log1 * 1000000)" | bc -l`
+	echo "HiCOO,GFLOPS - $GFLOPS" >> $out	
+
+done
+
+
+
